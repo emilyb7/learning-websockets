43a0bdce5e2c676111fa4b6b110ce355d95c9056
emily <limeyb7@gmail.com>
2017-04-20 18:28:17 +0100

    removes redundant socket container

--- app/app.jsx
+++ app/app.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 /* containers */
 import Game from './containers/game.jsx';
-import SocketManager from './containers/socket-manager.jsx';
 
 const App = () => {
 
   return (
     <div>
       <Game />
-      <SocketManager />
     </div>
   );
 };
