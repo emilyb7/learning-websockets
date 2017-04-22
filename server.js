2ce4d8d16b16bf0a644ec0049c1ca99606b9fd71
emily <limeyb7@gmail.com>
2017-04-22 12:24:28 +0100

    moves sockets code to own file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const ws = require('ws');
+const createSocket = require('./server/sockets.js');
 
 const PORT = process.env.PORT || 7000;
 
@@ -9,100 +9,4 @@ const server = express()
   .use('/css', express.static(__dirname + "/public"))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const w = new ws.Server({ server });
-
-w.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
-
-let queue = [];
-let members = null;
-
-const channel = (membersArr, message) => {
-  membersArr.forEach(member => {
-    member.send(message);
-  });
-};
-
-w.on('connection', (ws) => {
-  queue = queue.concat(ws);
-
-  // start game
-  if (queue.length === 1) {
-    ws.send(JSON.stringify({
-      type: "welcome",
-      message: "You've joined the game, waiting for another player to join",
-    }));
-  } else if (queue.length === 2) {
-    queue[0].send(JSON.stringify({
-      type: "NEW_PLAYERS",
-      message: "another player has joined the game",
-      playerid: 0,
-    }));
-    ws.send(JSON.stringify({
-      type: "NEW_PLAYERS",
-      message: "You've joined the game",
-      playerid: 1,
-    }));
-
-    // create channel
-    members = queue.slice(0, 2);
-    channel(members, JSON.stringify({
-      type: "init",
-      message: "game begins",
-    }));
-  } else {
-    ws.send(JSON.stringify({
-      type: "queue",
-      message: "you're in a queue to join the game",
-    }));
-  }
-
-  // handle user input
-  ws.on('message', (msg) => {
-    if (members) {
-      console.log(msg);
-      channel(members, msg);
-    }
-  });
-
-
-  ws.on('close', (evt) => {
-    let index = queue.indexOf(ws);
-    queue = queue.filter((_,i) => i !== index);
-    members = null;
-
-    // end currently running game
-    if (index < 2 && queue.length) {
-        queue[0].send(JSON.stringify({
-          type: "player-leaves",
-          message: "your opponent has left the game",
-        }));
-
-        // start a new game
-        if (queue[1]) {
-          queue[1].send(JSON.stringify({
-            type: "NEW-PLAYERS",
-            message: "you're on",
-            playerid: 1,
-          }));
-          queue[0].send(JSON.stringify({
-            type: "NEW-PLAYERS",
-            message: "a new player has joined",
-            playerid: 0,
-          }))
-          members = queue.slice(0, 2);
-          channel(members, {
-            type: "init",
-            message: "new game begins",
-          });
-        } else {
-          queue[0].send(JSON.stringify({
-            type: "waiting",
-            message: "waiting for a new player to join",
-          }));
-        }
-      }
-    });
-});
+createSocket(server);
---
+++ server/sockets.js
@@ -0,0 +1,102 @@
+const ws = require('ws');
+
+const channel = (membersArr, message) => {
+  membersArr.forEach(member => {
+    member.send(message);
+  });
+};
+
+module.exports = server => {
+
+  const w = new ws.Server({ server });
+
+  let queue = [];
+  let members = null;
+
+  w.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
+  });
+
+  w.on('connection', (ws) => {
+    queue = queue.concat(ws);
+
+    // start game
+    if (queue.length === 1) {
+      ws.send(JSON.stringify({
+        type: "welcome",
+        message: "You've joined the game, waiting for another player to join",
+      }));
+    } else if (queue.length === 2) {
+      queue[0].send(JSON.stringify({
+        type: "NEW_PLAYERS",
+        message: "another player has joined the game",
+        playerid: 0,
+      }));
+      ws.send(JSON.stringify({
+        type: "NEW_PLAYERS",
+        message: "You've joined the game",
+        playerid: 1,
+      }));
+
+      // create channel
+      members = queue.slice(0, 2);
+      channel(members, JSON.stringify({
+        type: "init",
+        message: "game begins",
+      }));
+    } else {
+      ws.send(JSON.stringify({
+        type: "queue",
+        message: "you're in a queue to join the game",
+      }));
+    }
+
+    // handle user input
+    ws.on('message', (msg) => {
+      if (members) {
+        console.log(msg);
+        channel(members, msg);
+      }
+    });
+
+
+    ws.on('close', (evt) => {
+      let index = queue.indexOf(ws);
+      queue = queue.filter((_,i) => i !== index);
+      members = null;
+
+      // end currently running game
+      if (index < 2 && queue.length) {
+          queue[0].send(JSON.stringify({
+            type: "player-leaves",
+            message: "your opponent has left the game",
+          }));
+
+          // start a new game
+          if (queue[1]) {
+            queue[1].send(JSON.stringify({
+              type: "NEW-PLAYERS",
+              message: "you're on",
+              playerid: 1,
+            }));
+            queue[0].send(JSON.stringify({
+              type: "NEW-PLAYERS",
+              message: "a new player has joined",
+              playerid: 0,
+            }))
+            members = queue.slice(0, 2);
+            channel(members, {
+              type: "init",
+              message: "new game begins",
+            });
+          } else {
+            queue[0].send(JSON.stringify({
+              type: "waiting",
+              message: "waiting for a new player to join",
+            }));
+          }
+        }
+      });
+  });
+}
